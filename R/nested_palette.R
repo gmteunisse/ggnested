f1941ac781434a6cdfda25b26765dc6b12e5d776
gmteunisse <gmteunisse@gmail.com>
2022-08-02 10:49:18 +0200

    Fixed group ordering when providing a custom palette/

--- R/nested_palette.R
+++ R/nested_palette.R
@@ -42,7 +42,7 @@ nested_palette <- function(data, group, subgroup, gradient_type = c("both", "sha
     stop(msg)
   }
   
-  # Order by group and the count of each subgroup
+  # Order by group and subgroup
   df <- data %>%
     arrange(!!sym(group), !!sym(subgroup)) %>%
     mutate(group_subgroup = sprintf("%s%s", !!sym(group), !!sym(subgroup)) %>%
@@ -62,20 +62,20 @@ nested_palette <- function(data, group, subgroup, gradient_type = c("both", "sha
     if (length(palette) < n_clrs){
       stop(sprintf("Error: %d values required in clr.pal, %d provided.", n_clrs, length(palette)))
     }
-    clr_pal <- palette[1:n_clrs]
+    clr_pal <- palette
   }
 
   # Assign group colours
   if (!is.null(names(clr_pal))){
     if (all(groups %in% names(clr_pal))){
-      group_colours <- data.frame(group = names(clr_pal),
-                                  group_colour = clr_pal)
+      group_colours <- data.frame(group = groups,
+                                  group_colour = unname(clr_pal[as.character(groups)]))
     } else {
       stop("Error: palette names don't correspond to group levels.")
     }
   } else {
     group_colours <- data.frame(group = groups,
-                                group_colour = clr_pal)
+                                group_colour = clr_pal[1:n_clrs])
   }
 
   # Rename to group name
